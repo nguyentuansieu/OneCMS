9c4cbbd85d9e6838ac764222c9f0d6912f08da6d
Nguyen Tuan Sieu <tuan@sieulog.com>
2015-12-21 07:51:42 +0000

    update

--- common/config/main-local.php
+++ common/config/main-local.php
@@ -14,7 +14,15 @@ return [
             // send all mails to a file by default. You have to set
             // 'useFileTransport' to false and configure a transport
             // for the mailer to send real emails.
-            'useFileTransport' => true,
+            'useFileTransport' => false,
+            'transport' => [
+              'class' => 'Swift_SmtpTransport',
+              'host' => 'smtp.sendgrid.net',
+              'username' => 'phutungoto',
+              'password' => 'Nguyen123@',
+              'port' => '465',
+              'encryption' => 'ssl',
+            ]
         ],
     ],
 ];
